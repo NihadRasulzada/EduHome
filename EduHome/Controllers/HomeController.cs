38e09f08b16aeafc9e8e965dc07a4cd698e373ed
NihadRasulzada <nihadrs2005@gmail.com>
2023-03-19 12:28:44 +0400

    AddSliderInSQL

---
+++ EduHome/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using EduHome.DAL;
+using EduHome.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduHome.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _db;
+        public HomeController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Slider> slider = _db.Sliders.ToList();
+            return View(slider);
+        }
+    }
+}
---
+++ EduHome/Controllers/RegisterController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EduHome.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
